c112e515460b5cb263e44b7d51db5124328aec46
Mr.Tang <geektcp@163.com>
2023-03-15 11:11:12 +0800

    init

---
+++ src/main/java/com/geektcp/app/ThyApp.java
@@ -0,0 +1,23 @@
+package com.geektcp.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Administrator on 2023/3/14 15:44.
+ */
+@Slf4j
+@EnableScheduling
+@EnableAsync
+@ComponentScan(lazyInit = true)
+@SpringBootApplication
+@ComponentScan({"com.*"})
+public class ThyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ThyApp.class, args);
+    }
+}
---
+++ src/main/java/com/geektcp/app/controller/ApiController.java
@@ -0,0 +1,24 @@
+package com.geektcp.app.controller;
+
+import com.geektcp.common.core.generator.IdGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator on 2023/3/15 10:00.
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/test")
+public class ApiController {
+
+    @GetMapping(path = "/id")
+    public String getId() {
+        String id = IdGenerator.getId("id_");
+        log.info("generator id: {}", id);
+        return id;
+    }
+
+}
